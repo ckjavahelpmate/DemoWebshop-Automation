bf547d279da9ec53f80f08aa31bbaa848227cd50
ckjavahelpmate <ck.javahelpmate@gmail.com>
2025-08-31 11:25:52 +0530

    Framework created

---
+++ src/test/java/com/tests/ecommerce/AmazonTest.java
@@ -0,0 +1,24 @@
+package com.tests.ecommerce;
+
+import org.testng.annotations.Test;
+
+import com.genericlibrary.BaseTest;
+
+public class AmazonTest extends BaseTest {
+
+	
+	@Test( groups = { "ecommerce", "smoke" , "regression"}  )
+	public void amazonAppOpen( ) 
+	{	
+		drivers.get().get("https://www.Amazon.com");
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+
+
+
---
+++ src/test/java/com/tests/food/ZomatoTest.java
@@ -0,0 +1,24 @@
+package com.tests.food;
+
+import org.testng.annotations.Test;
+
+import com.genericlibrary.BaseTest;
+
+
+public class ZomatoTest extends BaseTest {
+	
+	@Test( groups = {"food", "regression"})
+	public void zomatoAppOpen() {
+		
+		
+		drivers.get().get("https://www.zomato.com");
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
