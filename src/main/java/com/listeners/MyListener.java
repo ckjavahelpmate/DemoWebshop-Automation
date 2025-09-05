402bacdc737b84941315ff972396ba0217be97dd
ckjavahelpmate <ck.javahelpmate@gmail.com>
2025-09-05 22:56:51 +0530

    log

--- src/main/java/com/listeners/MyListener.java
+++ src/main/java/com/listeners/MyListener.java
@@ -14,24 +14,28 @@ public class MyListener implements ITestListener, ISuiteListener
 	@Override
 	public void onStart(ISuite suite) 
 	{
+		System.out.println("Suite Started");
 		ExtentReportManager.createExtentReport(suite.getName());
 	}
 	
 	@Override
 	public void onTestStart(ITestResult result)
 	{
+		System.out.println("Test Started");
 		ExtentReportManager.createTest( result.getName() );
 	}
 	
 	@Override
 	public void onTestSuccess(ITestResult result) 
 	{
+		System.out.println(result.getName() + " :: Test Passesd");
 		ExtentReportManager.pass(result.getName() + " Test Passed");
 	}
 	
 	@Override
 	public void onTestFailure(ITestResult result) 
 	{
+		
 		ExtentReportManager.fail( result.getThrowable().toString() );
 		String path = ScreenshotUtil.takeScreenshot("flipkartAppOpen");
 		ExtentReportManager.addScreenshot(path);
@@ -46,6 +50,7 @@ public class MyListener implements ITestListener, ISuiteListener
 	@Override
 	public void onFinish(ISuite suite)
 	{
+		System.out.println("Suite Ended");
 		ExtentReportManager.publishReport();
 	}
 
