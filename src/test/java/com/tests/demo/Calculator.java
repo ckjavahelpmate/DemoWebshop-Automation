147a5a347dc7f553ed97788053b58a627d71e5c6
rohiniHS-dev <rohinisiddesh1@gmail.com>
2026-01-21 11:30:21 +0530

    Updated calculator app

--- src/test/java/com/tests/demo/Calculator.java
+++ src/test/java/com/tests/demo/Calculator.java
@@ -1,11 +1,16 @@
 package com.tests.demo;
 
+import java.util.Scanner;
+
 public class Calculator {
 
 	public static void main(String[] args) 
 	{
-		int num1 = 25 ;
-		int num2 = 30 ;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter first num :");
+		int num1 = scan.nextInt();
+		System.out.println("Enter second num:");
+		int num2 = scan.nextInt();
 		int res = num1  + num2 ;
 		System.out.println("Result : "+ res );
 
