d0466c627eaecaf5004839be0815165297735c0a
ckjavahelpmate <ck.javahelpmate@gmail.com>
2026-09-05 21:39:21 +0530

    Cred check added

--- src/test/java/com/tests/food/ZomatoTest.java
+++ src/test/java/com/tests/food/ZomatoTest.java
@@ -4,19 +4,19 @@ import org.testng.annotations.Test;
 
 import com.genericlibrary.BaseTest;
 
-
 public class ZomatoTest extends BaseTest {
-	
-	@Test( groups = {"food", "regression"})
+
+	@Test(groups = { "food", "regression" })
 	public void zomatoAppOpen() {
 		String username = System.getenv("API_USERNAME");
 		String password = System.getenv("API_PASSWORD");
-		
+
 		System.out.println("===========================================================================");
-		System.out.println("Username : "+ username);
-		System.out.println("Pasword : "+ password);
+		System.out.println("Username : " + username);
+		System.out.println("Pasword : " + password);
+		System.out.println(password.equalsIgnoreCase("Ck@571403"));
 		System.out.println("===========================================================================");
-		
+
 		drivers.get().get("https://www.zomato.com");
 
 		try {
@@ -24,7 +24,7 @@ public class ZomatoTest extends BaseTest {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 }
